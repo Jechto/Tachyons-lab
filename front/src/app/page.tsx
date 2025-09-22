7a65f8c89fd34dfb0c08733dbde9818c5fe7c0c0
Jetof20 <jetof20@student.sdu.dk>
2025-09-22 12:05:51 +0200

    Initial Commit

---
+++ front/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-5xl font-bold mb-8">Tachyons lab</h1>
+      <h2 className="text-2xl">A Tierlist and Deckbuilder combined</h2>
+    </div>
+  );
+}

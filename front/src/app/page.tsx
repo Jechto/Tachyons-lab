d5518159ab085d7fe883e1ba1affe1cb3ac31c3c
Jetof20 <katten.saxo@gmail.com>
2025-09-22 12:49:19 +0200

    changed github actions

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-5xl font-bold mb-8">Tachyons lab</h1>
       <h2 className="text-2xl">A Tierlist and Deckbuilder combined</h2>
+      
     </div>
   );
 }

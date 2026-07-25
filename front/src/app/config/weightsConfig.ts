e1e87f4e2a42ebbe30c29b2736450616f1c91f85
Jechto <katten.saxo@gmail.com>
2026-07-26 01:12:26 +0200

    Changing weights to decrease stamina requirements

--- front/src/app/config/weightsConfig.ts
+++ front/src/app/config/weightsConfig.ts
@@ -11,30 +11,30 @@ export const WEIGHTS_CONFIG = {
     },
     Mile: {
         Speed: 1.5,
-        Stamina: 0.75,
+        Stamina: 0.25,
         Power: 1.5,
-        Guts: 0.5,
-        Wit: 0.75,
+        Guts: 0.75,
+        Wit: 1,
         "Skill Points": 0.4,
         Hints: 7.5, // Hint is equal to 6 speed+ 2 Power points
         "Gold Skills": 1.0, // Value for gold (rarity 2) skill hints
     },
     Medium: {
         Speed: 1.5,
-        Stamina: 1.0,
+        Stamina: 0.5,
         Power: 1.0,
-        Guts: 0.75,
-        Wit: 0.75,
+        Guts: 1.0,
+        Wit: 1,
         "Skill Points": 0.4,
         Hints: 7.5, // Hint is equal to 6 speed+ 2 Power points
         "Gold Skills": 1.0, // Value for gold (rarity 2) skill hints
     },
     Long: {
         Speed: 1.25,
-        Stamina: 1.25,
+        Stamina: 1.0,
         Power: 0.5,
         Guts: 1.25,
-        Wit: 0.75,
+        Wit: 1,
         "Skill Points": 0.4,
         Hints: 7.5, // Hint is equal to 6 speed+ 2 Power points
         "Gold Skills": 1.0, // Value for gold (rarity 2) skill hints
